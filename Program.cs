450c9ffa8a6f851815afa1185fea1e5728dbb729
Prozherin <taskacik@gmail.com>
2023-10-08 12:54:11 +0500

    Завершённый код ver1.0

--- Program.cs
+++ Program.cs
@@ -1,20 +1,50 @@
-﻿static string[] FilterStrings(string[] array)
+﻿Console.WriteLine("Введите элементы массива через запятую:");
+string input = Console.ReadLine();
+
+string[] inputArray = input.Split(',');
+
+string[] filteredArray = FilterStrings(inputArray);
+
+Console.WriteLine("Результат:");
+Console.WriteLine(string.Join(", ", filteredArray));
+
+string[] FilterStrings(string[] array)
 {
     int count = CountFilteredStrings(array);
     string[] filteredArray = new string[count];
     int index = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (IsStringValid(array[i]))
+        {
+            filteredArray[index] = array[i];
+            index++;
+        }
+    }
+
+    Array.Resize(ref filteredArray, index); // Уменьшаем размер массива до реального количества элементов
+
     return filteredArray;
 }
 
-static int CountFilteredStrings(string[] array)
+int CountFilteredStrings(string[] array)
 {
     int count = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (IsStringValid(array[i]))
+        {
+            count++;
+        }
+    }
+
     return count;
 }
 
-static bool IsStringValid(string str)
+bool IsStringValid(string str)
 {
-
-
-    return false;
+    // Проверка условий для фильтрации строки
+    return str.Length <= 3;
 }
