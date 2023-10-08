13220c6d1432c23b44c0630982c118e35a34c34b
Prozherin <taskacik@gmail.com>
2023-10-08 10:28:35 +0500

    IsStringValid

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿static string[] FilterStrings(string[] array)
+{
+    int count = CountFilteredStrings(array);
+    string[] filteredArray = new string[count];
+    int index = 0;
+    return filteredArray;
+}
+
+static int CountFilteredStrings(string[] array)
+{
+    int count = 0;
+    return count;
+}
+
+static bool IsStringValid(string str)
+{
+
+
+    return false;
+}
