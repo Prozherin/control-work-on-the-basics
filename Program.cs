6d20783cb83d03e36d8be60f75b8243d19c83646
Prozherin <taskacik@gmail.com>
2023-10-09 22:18:03 +0500

    Добавили коментарии

--- Program.cs
+++ Program.cs
@@ -1,31 +1,31 @@
-﻿Console.WriteLine("Введите элементы массива через запятую:");
-string input = Console.ReadLine();
+﻿Console.WriteLine("Введите элементы массива через запятую:"); // Вывод просьбы ввести элементы массива
+string input = Console.ReadLine(); // Чтение введенной строки с клавиатуры
 
-string[] inputArray = input.Split(',');
+string[] inputArray = input.Split(','); // Разделение введенной строки на элементы массива по запятой
 
-string[] filteredArray = FilterStrings(inputArray);
+string[] filteredArray = FilterStrings(inputArray); // Фильтрация массива строк
 
-Console.WriteLine("Результат:");
-Console.WriteLine(string.Join(", ", filteredArray));
+Console.WriteLine("Результат:"); // Вывод заголовка результата
+Console.WriteLine(string.Join(", ", filteredArray)); // Вывод отфильтрованных строк, объединенных запятой
 
 string[] FilterStrings(string[] array)
 {
-    int count = CountFilteredStrings(array);
-    string[] filteredArray = new string[count];
+    int count = CountFilteredStrings(array); // Подсчет количества отфильтрованных строк
+    string[] filteredArray = new string[count]; // Создание нового массива для отфильтрованных строк
     int index = 0;
 
     for (int i = 0; i < array.Length; i++)
     {
-        if (IsStringValid(array[i]))
+        if (IsStringValid(array[i])) // Проверка, является ли строка допустимой
         {
-            filteredArray[index] = array[i];
+            filteredArray[index] = array[i]; // Добавление строки в отфильтрованный массив
             index++;
         }
     }
 
-    Array.Resize(ref filteredArray, index); // Уменьшаем размер массива до реального количества элементов
+    Array.Resize(ref filteredArray, index); // Уменьшение размера массива до реального количества элементов
 
-    return filteredArray;
+    return filteredArray; // Возвращение отфильтрованного массива
 }
 
 int CountFilteredStrings(string[] array)
@@ -34,17 +34,17 @@ int CountFilteredStrings(string[] array)
 
     for (int i = 0; i < array.Length; i++)
     {
-        if (IsStringValid(array[i]))
+        if (IsStringValid(array[i])) // Проверка, является ли строка допустимой
         {
-            count++;
+            count++; // Увеличение счетчика отфильтрованных строк
         }
     }
 
-    return count;
+    return count; // Возвращение количества отфильтрованных строк
 }
 
 bool IsStringValid(string str)
 {
     // Проверка условий для фильтрации строки
-    return str.Length <= 3;
+    return str.Length <= 3; // Возвращение значения, указывающего, является ли строка допустимой
 }
